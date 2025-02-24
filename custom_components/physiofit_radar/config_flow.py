7b5a3f80e15b8a5a543ef577669db90ec148bde7
Schimi42 <michell.schimanski@gmail.com>
2025-02-24 16:42:04 +0100

    Config Flow

--- custom_components/physiofit_radar/config_flow.py
+++ custom_components/physiofit_radar/config_flow.py
@@ -15,7 +15,6 @@ STEP_USER_DATA_SCHEMA = vol.Schema({})
 
 async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
     """Validate the user input allows us to connect."""
-    # In this case, we don't need to validate anything as we don't have any user input
     return {"title": "PhysioFIT Auslastungsradar"}
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -27,20 +26,17 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
+        # Check if already configured
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured()
+
         if user_input is None:
             return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+                step_id="user",
+                data_schema=STEP_USER_DATA_SCHEMA,
             )
 
-        errors = {}
-
-        try:
-            info = await validate_input(self.hass, user_input)
-        except Exception:  # pylint: disable=broad-except
-            errors["base"] = "unknown"
-        else:
-            return self.async_create_entry(title=info["title"], data=user_input)
-
-        return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        return self.async_create_entry(
+            title="PhysioFIT Auslastungsradar",
+            data={}
         ) 
